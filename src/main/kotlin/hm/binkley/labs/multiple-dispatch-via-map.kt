9b022a802228ab84a86673fe97929a53e8e8dd7d
Brian Oxley <boxley@thoughtworks.com>
2021-04-22 14:37:13 +0800

    Fixes #2.  Less magic

--- src/main/kotlin/hm/binkley/labs/multiple-dispatch-via-map.kt
+++ src/main/kotlin/hm/binkley/labs/multiple-dispatch-via-map.kt
@@ -16,12 +16,15 @@ internal inline fun <reified D : Device<D>, reified C : Command<C>> register(
     dispatchTable[key] = lambda as (Device<*>, Command<*>) -> Int
 }
 
-internal inline fun <reified D : Device<D>, reified C : Command<C>> dispatch(
+internal fun <D : Device<D>, C : Command<C>> dispatch(
     device: D,
     command: C,
 ): Int {
+    val deviceType = device::class
+    val commandType = command::class
+
     @Suppress("TYPE_INFERENCE_ONLY_INPUT_TYPES_WARNING")
-    val lambda = dispatchTable.get(D::class to C::class)
+    val lambda = dispatchTable.get(deviceType to commandType)
         ?: throw MissingMethodException(device, command)
 
     return lambda(device, command)
